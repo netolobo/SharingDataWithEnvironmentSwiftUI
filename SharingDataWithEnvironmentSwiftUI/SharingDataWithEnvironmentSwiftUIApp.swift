8594d1edaca864342780f00445545ef858198d1d
netolobo <desilio@gmail.com>
2024-02-12 20:05:19 -0800

    Initial Commit

---
+++ SharingDataWithEnvironmentSwiftUI/SharingDataWithEnvironmentSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharingDataWithEnvironmentSwiftUIApp.swift
+//  SharingDataWithEnvironmentSwiftUI
+//
+//  Created by Neto Lobo on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SharingDataWithEnvironmentSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
